19fd7381b9014217fce5f05a9d87ae3523024a6d
Kholkins <kholkins@mail.ru>
2017-10-05 13:58:53 +0300

    rev

--- pages/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from pages.models import Vacancie, Product, PurchasesPrice
-
-# Register your models here.
-
-admin.site.register(Vacancie)
-admin.site.register(Product)
-admin.site.register(PurchasesPrice)
