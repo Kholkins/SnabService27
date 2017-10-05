2f179927022b8ea023e81da0aff9885db68db736
Kholkins <kholkins@mail.ru>
2017-10-05 13:49:50 +0300

    urls

--- SnabService/urls.py
+++ SnabService/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'pages.views.IndexView', name='index'),
+    url(r'^sales/', 'pages.views.SalesView', name='sales'),
+    url(r'^purchases/', 'pages.views.PurchasesView', name='purchases'),
+    url(r'^vacancies/', 'pages.views.VacanciesView', name='vacancies'),
+    url(r'^contacts/', 'pages.views.ContactsView', name='contacts'),
     url(r'^admin/', admin.site.urls),
 ]
