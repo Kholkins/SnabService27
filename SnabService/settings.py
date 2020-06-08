44c29fd765ee609b34126598b8ecd22ebb552491
Kholkins <kholkins@mail.ru>
2020-06-08 19:30:20 +0300

    Update settings.py

--- SnabService/settings.py
+++ SnabService/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_)ujx*!mev_qx!n7crpgo34n4q-e(+=8c!xab2s2boi#k28fc#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
